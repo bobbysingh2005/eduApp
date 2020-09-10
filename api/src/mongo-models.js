b44d3b016dadd7fd59ba04b6513f80c99c99e24f
bobbysingh2005 <bobbysingh2005@gmail.com>
2020-09-10 16:53:58 +0530

    clean

--- api/src/mongo-models.js
+++
@@ -1,4 +0,0 @@
-import db from './db';
-import { UserSchema } from './mongo-schema';
-
-export const Users = db.model('users', UserSchema);
--- api/src/mongo-schema.js
+++
@@ -1,9 +0,0 @@
-import { Schema } from "mongoose";
-
-
-
-export const UserSchema = new Schema({
-    userId: String,
-    password: String,
-    rePassword: String
-});//endSchema;
