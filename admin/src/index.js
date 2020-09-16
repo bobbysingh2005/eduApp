71a67931fbc21e5ce1f35f5ff7111b3ab7bf316f
bobbysingh2005 <bobbysingh2005@gmail.com>
2020-09-17 00:43:17 +0530

    clear

--- admin/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import reactDom from 'react-dom';
-
-import App from './app';
-
-reactDom.render(<App />, document.getElementById('root'));
-
-alert_'hello bobby');
--- api/src/index.js
+++
@@ -1,5 +0,0 @@
-
-import App from './server.js';
-const port = process.env.PORT || 3000;
-
-App.listen(port, ()=>console.log(`edu api server started on http://localhost:${port} \n press ctrl + c to exit`));
